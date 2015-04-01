b4e898b624366e70843aeae9e1e621c8806dbf59
anderskristo <anderskristo@gmail.com>
2015-04-01 22:28:43 +0200

    count up and running

--- ngCountUp.js
+++ ngCountUp.js
@@ -1,4 +1,39 @@
 angular.module('ngCountUp', []).
-  directive('ngCountUp', function($q, $timeout) {
-    
+  directive('ngCountUp', function($timeout) {
+
+    var linker = function ($scope, element, attrs) {
+
+      // Create countUp
+      var count = new countUp(
+        element[0],
+        $scope.from,
+        $scope.to,
+        $scope.decimals,
+        $scope.duration,
+        $scope.options
+      );
+
+      // Run after delay
+      if ($scope.delay) {
+        $timeout(function() {
+          count.start();
+        }, $scope.delay);
+      } else {
+        count.start();
+      }
+    };
+
+    return {
+      restrict: 'E',
+      scope: {
+        from: '=',
+        to: '=',
+        decimals: '=',
+        duration: '=',
+        delay: '=',
+        options: '='
+      },
+      link: linker
+    };
+
   });
