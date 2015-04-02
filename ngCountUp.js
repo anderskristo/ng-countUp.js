600f6793975816b44430ec065a05de6bb89177f9
anderskristo <anderskristo@gmail.com>
2015-04-02 22:26:27 +0200

    build

--- ngCountUp.js
+++ ngCountUp.js
@@ -2,10 +2,11 @@ angular.module('ngCountUp', []).
   directive('ngCountUp', function($timeout) {
 
     var linker = function ($scope, element, attrs) {
+      var elem = element[0];
 
       // Create countUp
       var count = new countUp(
-        element[0],
+        elem,
         $scope.from,
         $scope.to,
         $scope.decimals,
@@ -15,7 +16,9 @@ angular.module('ngCountUp', []).
 
       // Run after delay
       if ($scope.delay) {
+        angular.element(elem).addClass('ng-hide');      // Hide countUp if delay exist
         $timeout(function() {
+          angular.element(elem).removeClass('ng-hide'); // Display countUp when delay has ended
           count.start();
         }, $scope.delay);
       } else {
