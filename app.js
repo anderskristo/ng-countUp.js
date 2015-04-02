35c35aa50acd13c9f413a25aa985d73f93366afc
anderskristo <anderskristo@gmail.com>
2015-04-02 22:16:38 +0200

    stuff

---
+++ app.js
@@ -0,0 +1,16 @@
+'use strict';
+
+angular.module('App', [
+  'ngCountUp'
+])
+
+.controller('AppCtrl', ['$scope', function($scope) {
+  $scope.options = {
+    useEasing : true,
+    useGrouping : true,
+    separator : ',',
+    decimal : '.',
+    prefix : '',
+    suffix : ''
+  };
+}]);
