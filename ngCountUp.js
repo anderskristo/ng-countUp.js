6772f9b5b77223c50758942ce06f6039649208f9
anderskristo <anderskristo@gmail.com>
2015-04-04 10:47:01 +0200

    Added watchCollection to make it more usable :star:

--- ngCountUp.js
+++ ngCountUp.js
@@ -2,41 +2,54 @@ angular.module('ngCountUp', []).
   directive('ngCountUp', ['$timeout', function($timeout) {
 
     var linker = function ($scope, element, attrs) {
-      var elem = element[0];
+      // Set default values
+      $scope.from = 0;
+      $scope.to = 0;
+      $scope.decimals = 0;
+      $scope.duration = 0;
+      $scope.delay = 0;
 
-      // Create countUp
-      var count = new countUp(
-        elem,
-        $scope.from,
-        $scope.to,
-        $scope.decimals,
-        $scope.duration,
-        $scope.options
-      );
+      $scope.$watchCollection('[from, to, decimals, duration, delay]', function(newValues, oldValues) {
+        if (newValues) {
+          var elem = element[0];
 
-      // Run after delay
-      if ($scope.delay) {
-        angular.element(elem).addClass('ng-hide');      // Hide countUp if delay exist
-        $timeout(function() {
-          angular.element(elem).removeClass('ng-hide'); // Display countUp when delay has ended
-          count.start();
-        }, $scope.delay);
-      } else {
-        count.start();
-      }
+          // Create countUp
+          var count = new countUp(
+            elem,
+            $scope.from,
+            $scope.to,
+            $scope.decimals,
+            $scope.duration,
+            $scope.options
+          );
+
+          // Run after delay
+          if ($scope.delay) {
+            angular.element(elem).addClass('ng-hide');      // Hide countUp if delay exist
+            $timeout(function() {
+              angular.element(elem).removeClass('ng-hide'); // Display countUp when delay has ended
+              count.start();
+            }, $scope.delay);
+          } else {
+            count.start();
+          }
+        } else {
+
+        }
+      });
     };
 
     return {
       restrict: 'E',
       scope: {
-        from: '=',
-        to: '=',
-        decimals: '=',
-        duration: '=',
-        delay: '=',
+        from: '@',
+        to: '@',
+        decimals: '@',
+        duration: '@',
+        delay: '@',
         options: '='
       },
       link: linker
     };
 
-  });
+  }]);
